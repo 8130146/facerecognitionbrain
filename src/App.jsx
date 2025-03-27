dbb90d6b713b807abab5a51bb662f45fd884af7a
Jose Sousa <8130146@estg.ipp.pt>
2025-03-27 16:51:16 +0000

    Added Profile and tokens

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,25 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Signin from './components/Signin/Signin'
 import Register from './components/Register/Register'
 import ParticlesBg from 'particles-bg'
+import Modal from './components/Modal/Modal'
+import Profile from './components/Profile/Profile'
 import './App.css'
 
 const initialState = {
   input: '',
   imageUrl: '',
   boxes: [],
-  route: 'home',
-  isSignedIn: true,
+  route: 'signin',
+  isSignedIn: false,
+  isProfileOpen: false,
   user:{
     id: '',
     name: '',
     email: '',
     entries: 0,
-    joined: ''
+    joined: '',
+    pet : '',
+    age : ''
   }
 }
 
@@ -28,6 +33,39 @@ class App extends Component{
   constructor(){
     super()
     this.state = initialState;
+  }
+
+  componentDidMount(){
+    const token = window.sessionStorage.getItem('token');
+    if(token){
+      fetch('http://localhost:3000/signin', {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json',
+          'Authorization': token
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if(data && data.id){
+          fetch(`http://localhost:3000/profile/${data.id}`, {
+            method: 'get',
+            headers: {
+              'content-type': 'application/json',
+              'Authorization': token
+            }
+          })
+          .then(response => response.json())
+          .then(user => {
+            if(user && user.email){
+              this.loadUser(user)
+              this.onRouteChange('home')
+            }
+          })
+        }
+      })
+      .catch(console.log)
+    }
   }
 
   loadUser = (data) => {
@@ -71,7 +109,10 @@ class App extends Component{
     // fetch('https://smartbrain-api-fazh.onrender.com/imageurl', {
       fetch('http://localhost:3000/imageurl', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': window.sessionStorage.getItem('token')
+      },
       body: JSON.stringify({
         input: this.state.input
       })
@@ -82,7 +123,10 @@ class App extends Component{
         // fetch('https://smartbrain-api-fazh.onrender.com/image', {
           fetch('http://localhost:3000/image', {
           method: 'put',
-          headers: {'Content-Type': 'application/json'},
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': window.sessionStorage.getItem('token')
+          },
           body: JSON.stringify({
             id: this.state.user.id
           })
@@ -108,12 +152,24 @@ class App extends Component{
     this.setState({route: route})
   }
 
+  toggleModal = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      isProfileOpen: !prevState.isProfileOpen
+    }))
+  }
+
   render(){
-    const { isSignedIn, imageUrl, boxes, route } = this.state;
+    const { isSignedIn, imageUrl, boxes, route, isProfileOpen, user } = this.state;
     return (
       <div className='App'>
         <ParticlesBg type="square" bg={true} num={30}/>
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} toggleModal={this.toggleModal} />
+        { isProfileOpen &&
+          <Modal>
+            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} user={user} loadUser={this.loadUser}/>
+          </Modal>
+        }
         { route === 'home' 
         ? <div>
             <Logo />
